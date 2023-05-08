5608b4104c8fd95606f24c4e9ab387de38829e22
Steven Hill <steven@stevenhill.co.za>
2023-05-08 14:39:18 +0200

    Chapter 1

--- 01_lesson/src/app/store.js
+++ 01_lesson/src/app/store.js
@@ -1,8 +1,9 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
-    reducer: {
-        counter: counterReducer,
-    }
-})
+  // Place reducers you create in the reducer object
+  reducer: {
+    counter: counterReducer,
+  },
+});
--- 01_lesson/src/features/counter/counterSlice.js
+++ 01_lesson/src/features/counter/counterSlice.js
@@ -1,28 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    count: 0
-}
+  count: 0,
+};
 
 export const counterSlice = createSlice({
-    name: 'counter',
-    initialState,
-    reducers: {
-        increment: (state) => {
-            state.count += 1;
-        },
-        decrement: (state) => {
-            state.count -= 1;
-        },
-        reset: (state) => {
-            state.count = 0;
-        },
-        incrementByAmount: (state, action) => {
-            state.count += action.payload;
-        }
-    }
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+    reset: (state) => {
+      state.count = 0;
+    },
+    incrementByAmount: (state, action) => {
+      state.count += action.payload;
+    },
+  },
 });
 
-export const { increment, decrement, reset, incrementByAmount } = counterSlice.actions;
+// Need to export all actions and the reducer
+// Destructuring the actions from counterSlice.actions
+export const { increment, decrement, reset, incrementByAmount } =
+  counterSlice.actions;
 
+// Exporting the reducer, because the Redux Store will need it
 export default counterSlice.reducer;
